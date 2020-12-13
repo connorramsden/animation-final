e7c5de4b9e1f2699d04204ad35215e582897b773
Colton Soneson <colton.soneson@mymail.champlain.edu>
2020-12-13 06:45:38 -0500

    more updates, almost there, the nodes are spazzing though

--- AnimFinal/Assets/Scripts/SkeletalHandler.cs
+++ AnimFinal/Assets/Scripts/SkeletalHandler.cs
@@ -9,12 +9,13 @@ public class SkeletalHandler : MonoBehaviour
 	public Transform RightOculus;
 	public Transform HeadsetOculus;
 	public int iterations = 3;
+	public float effectorTolerance = 0.2f;
 
-	private List<GameObject> LeftArm;
-	private List<GameObject> RightArm;
-	private List<GameObject> LeftLeg;
-	private List<GameObject> RightLeg;
-	private List<GameObject> Spine;
+	private List<int> LeftArm;
+	private List<int> RightArm;
+	private List<int> LeftLeg;
+	private List<int> RightLeg;
+	private List<int> Spine;
 	private float HipToGroundHeight;
 
 	List<float> LeftArmBoneLengths;
@@ -41,7 +42,18 @@ public class SkeletalHandler : MonoBehaviour
 
 	void Start()
     {
-		if (OVRSkeletonNodes.Count <= 0)
+		LeftArm = new List<int>();
+		RightArm = new List<int>();
+		LeftLeg = new List<int>();
+		RightLeg = new List<int>();
+		Spine = new List<int>();
+		LeftArmBoneLengths = new List<float>();
+		RightArmBoneLengths = new List<float>();
+		LeftLegBoneLengths = new List<float>();
+		RightLegBoneLengths = new List<float>();
+		SpineBoneLengths = new List<float>();
+
+		if (OVRSkeletonNodes.Count > 0)
 		{
 			solveHipToGround();
 			fillJointLists();
@@ -51,13 +63,14 @@ public class SkeletalHandler : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-		updateFullSkeletonPostion();
+		//updateFullSkeletonPostion();
+		updateEffectorDistances();
 
-		SolveInverseKinematicsIter(ref LeftArm, LeftArmBoneLengths, LeftOculus.position, LeftArmEffectorDistance, LeftArmChainLength, iterations);
-		SolveInverseKinematicsIter(ref RightArm, RightArmBoneLengths, RightOculus.position, RightArmEffectorDistance, RightArmChainLength, iterations);
-		SolveInverseKinematicsIter(ref LeftLeg, LeftLegBoneLengths, LeftLegTarget(), LeftLegEffectorDistance, LeftLegChainLength, iterations);
-		SolveInverseKinematicsIter(ref RightLeg, RightLegBoneLengths, RightLegTarget(), RightLegEffectorDistance, RightLegChainLength, iterations);
-		SolveInverseKinematicsIter(ref Spine, SpineBoneLengths, HeadsetOculus.position, SpineEffectorDistance, SpineChainLength, iterations);
+		SolveInverseKinematicsIter(LeftArm, LeftArmBoneLengths, LeftOculus.position, LeftArmEffectorDistance, LeftArmChainLength, iterations);
+		//SolveInverseKinematicsIter(RightArm, RightArmBoneLengths, RightOculus.position, RightArmEffectorDistance, RightArmChainLength, iterations);
+		//SolveInverseKinematicsIter(LeftLeg, LeftLegBoneLengths, LeftLegTarget(), LeftLegEffectorDistance, LeftLegChainLength, iterations);
+		//SolveInverseKinematicsIter(RightLeg, RightLegBoneLengths, RightLegTarget(), RightLegEffectorDistance, RightLegChainLength, iterations);
+		//SolveInverseKinematicsIter(Spine, SpineBoneLengths, HeadsetOculus.position, SpineEffectorDistance, SpineChainLength, iterations);
     }
 
 	void solveHipToGround()
@@ -68,49 +81,60 @@ public class SkeletalHandler : MonoBehaviour
 	void fillJointLists()
 	{
 		//left arm
-		LeftArm.Add(OVRSkeletonNodes[findInSkeleton("LeftShoulder")]);
-		LeftArm.Add(OVRSkeletonNodes[findInSkeleton("LeftElbow")]);
-		LeftArm.Add(OVRSkeletonNodes[findInSkeleton("LeftHand")]);
-		
+		LeftArm.Add(findInSkeleton("LeftShoulder"));
+		LeftArm.Add(findInSkeleton("LeftElbow"));
+		LeftArm.Add(findInSkeleton("LeftHand"));
+
+		/*
+		for(int i = 0; i < LeftArm.Count; ++i)
+        {
+			Debug.Log("[OUR CODE] " + OVRSkeletonNodes[LeftArm[i]].name);
+        }
+		*/
 
 		//right arm
-		RightArm.Add(OVRSkeletonNodes[findInSkeleton("RightShoulder")]);
-		RightArm.Add(OVRSkeletonNodes[findInSkeleton("RightElbow")]);
-		RightArm.Add(OVRSkeletonNodes[findInSkeleton("RightHand")]);
+		RightArm.Add(findInSkeleton("RightShoulder"));
+		RightArm.Add(findInSkeleton("RightElbow"));
+		RightArm.Add(findInSkeleton("RightHand"));
 		
 
 		//left leg
-		LeftLeg.Add(OVRSkeletonNodes[findInSkeleton("LeftHip")]);
-		LeftLeg.Add(OVRSkeletonNodes[findInSkeleton("LeftKnee")]);
-		LeftLeg.Add(OVRSkeletonNodes[findInSkeleton("LeftFoot")]);
+		LeftLeg.Add(findInSkeleton("LeftHip"));
+		LeftLeg.Add(findInSkeleton("LeftKnee"));
+		LeftLeg.Add(findInSkeleton("LeftFoot"));
 
 
 		//right leg
-		RightLeg.Add(OVRSkeletonNodes[findInSkeleton("RightHip")]);
-		RightLeg.Add(OVRSkeletonNodes[findInSkeleton("RightKnee")]);
-		RightLeg.Add(OVRSkeletonNodes[findInSkeleton("RightFoot")]);
+		RightLeg.Add(findInSkeleton("RightHip"));
+		RightLeg.Add(findInSkeleton("RightKnee"));
+		RightLeg.Add(findInSkeleton("RightFoot"));
 
 
 		//spine (has to go from hip up)
-		Spine.Add(OVRSkeletonNodes[findInSkeleton("Hip")]);
-		Spine.Add(OVRSkeletonNodes[findInSkeleton("Spine")]);
-		Spine.Add(OVRSkeletonNodes[findInSkeleton("Neck")]);
-		Spine.Add(OVRSkeletonNodes[findInSkeleton("Head")]);
+		Spine.Add(findInSkeleton("Hip"));
+		Spine.Add(findInSkeleton("Spine"));
+		Spine.Add(findInSkeleton("Neck"));
+		Spine.Add(findInSkeleton("Head"));
 
 
 		//calculate chain length after jointlists are filled
-		fillJointListBoneAndChainLengths(LeftArm, ref LeftArmBoneLengths);
-		fillJointListBoneAndChainLengths(RightArm, ref RightArmBoneLengths);
-		fillJointListBoneAndChainLengths(LeftLeg, ref LeftLegBoneLengths);
-		fillJointListBoneAndChainLengths(RightLeg, ref RightLegBoneLengths);
-		fillJointListBoneAndChainLengths(Spine, ref SpineBoneLengths);
+		fillJointListBoneAndChainLengths(LeftArm, ref LeftArmBoneLengths, ref LeftArmChainLength);
+		fillJointListBoneAndChainLengths(RightArm, ref RightArmBoneLengths, ref RightArmChainLength);
+		fillJointListBoneAndChainLengths(LeftLeg, ref LeftLegBoneLengths, ref LeftLegChainLength);
+		fillJointListBoneAndChainLengths(RightLeg, ref RightLegBoneLengths, ref RightLegChainLength);
+		fillJointListBoneAndChainLengths(Spine, ref SpineBoneLengths, ref SpineChainLength);
+
+		/*
+		for(int i = 0; i < LeftArmBoneLengths.Count; ++i)
+        {
+			Debug.Log("[OUR CODE] LeftArmBoneLengths: " + LeftArmBoneLengths[i].ToString());
+        }
+		Debug.Log("[OUR CODE] LeftArmChainLength: " + LeftArmChainLength);
+		*/
+
 
 		//calculate effector distance
-		LeftArmEffectorDistance = (LeftArm[0].transform.position - LeftArm[(LeftArm.Count - 1)].transform.position).magnitude;
-		RightArmEffectorDistance = (RightArm[0].transform.position - RightArm[(RightArm.Count - 1)].transform.position).magnitude;
-		LeftLegEffectorDistance = (LeftLeg[0].transform.position - LeftLeg[(LeftLeg.Count - 1)].transform.position).magnitude;
-		RightLegEffectorDistance = (RightLeg[0].transform.position - RightLeg[(RightLeg.Count - 1)].transform.position).magnitude;
-		SpineEffectorDistance = (Spine[0].transform.position - Spine[(Spine.Count - 1)].transform.position).magnitude;
+		//updateEffectorDistances();
 		
 	}
 
@@ -118,29 +142,35 @@ public class SkeletalHandler : MonoBehaviour
     {
 		int result = 0;
 
-		for(int i = 0; i < OVRSkeletonNodes.Count - 1; ++i)
+		for(int i = 0; i < OVRSkeletonNodes.Count; ++i)
         {
 			if(OVRSkeletonNodes[i].gameObject.name == jointName)
             {
 				result = i;
-				break;
+				//Debug.Log("[OUR CODE] findInSkeleton: " + OVRSkeletonNodes[i].gameObject.name + " at Position #" + i);
+				return result;
             }
         }
 
 		return result;
     }
 
-	void fillJointListBoneAndChainLengths(List<GameObject> targetNodes, ref List<float> distances)
+	void fillJointListBoneAndChainLengths(List<int> targetNodes, ref List<float> boneLengths, ref float chainLength)
     {
 		if (targetNodes.Count != 0)
 		{
-			for (int i = 0; i < targetNodes.Count; i++)
+			for (int i = 0; i < targetNodes.Count - 1; i++)
 			{
-				Vector3 targetPos = targetNodes[i].transform.position;
-				Debug.DrawLine(this.transform.position, targetPos, Color.red, 5f);
+				if (i + 1 < targetNodes.Count)
+				{
+					Vector3 currNodePos = OVRSkeletonNodes[targetNodes[i]].transform.position;
+					Vector3 nextNodePos = OVRSkeletonNodes[targetNodes[i + 1]].transform.position;
 
-				//Debug.Log("Bone Length: " + (targetPos - this.transform.position).magnitude);
-				distances.Add((targetPos - this.transform.position).magnitude);
+					float dif = (nextNodePos - currNodePos).magnitude;
+					chainLength += dif;
+					boneLengths.Add(dif);
+
+				}
 			}
 		}
 		else
@@ -161,53 +191,85 @@ public class SkeletalHandler : MonoBehaviour
 
 	Vector3 LeftLegTarget()
     {
-		return (LeftLeg[0].transform.position - new Vector3(0.0f, HipToGroundHeight, 0.0f));
+		return (OVRSkeletonNodes[LeftLeg[0]].transform.position - new Vector3(0.0f, HipToGroundHeight, 0.0f));
 	}
 
 	Vector3 RightLegTarget()
 	{
-		return (RightLeg[0].transform.position - new Vector3(0.0f, HipToGroundHeight, 0.0f));
+		return (OVRSkeletonNodes[RightLeg[0]].transform.position - new Vector3(0.0f, HipToGroundHeight, 0.0f));
 	}
 
-	void SolveInverseKinematicsIter(ref List<GameObject> jointList, List<float> distances, Vector3 targetPos, float effectorDist, float chainLength, int iterations)
+	void updateEffectorDistances()
+    {
+		LeftArmEffectorDistance = (OVRSkeletonNodes[LeftArm[0]].transform.position - LeftOculus.position).magnitude;
+		RightArmEffectorDistance = (OVRSkeletonNodes[RightArm[0]].transform.position - RightOculus.position).magnitude;
+		LeftLegEffectorDistance = (OVRSkeletonNodes[LeftLeg[0]].transform.position - LeftLegTarget()).magnitude;
+		RightLegEffectorDistance = (OVRSkeletonNodes[RightLeg[0]].transform.position - RightLegTarget()).magnitude;
+		SpineEffectorDistance = (OVRSkeletonNodes[Spine[0]].transform.position - HeadsetOculus.position).magnitude;
+
+		//Debug.Log("[OUR CODE] LeftArmEffectorDistance: " + LeftArmEffectorDistance.ToString());
+	}
+
+	void SolveInverseKinematicsIter(List<int> jointList, List<float> distances, Vector3 targetPos, float effectorDist, float chainLength, int iterations)
 	{
-		if (effectorDist >= chainLength)
+		//Debug.Log("[OUR CODE] ED: " + effectorDist.ToString() + "  CL: " + chainLength.ToString());
+
+		if (effectorDist < chainLength)
 		{
-			return;
-		}
 
-		int iter = 0;
-		if (iterations <= 0)
-		{
-			iter = 1;
-		}
-
-		Vector3 firstBasePos = jointList[0].transform.position;    //record original root location
-
-		for (int i = 0; i < iter; ++i)
-		{
-			jointList[(jointList.Count - 1)].transform.position  = targetPos;    //move the endEffector to the target
-
-			//go through the chain from end to beginning
-			for (int j = (jointList.Count - 1); j > 1; --j)     //has to be this weird way for parents
+			int iter = 0;
+			if (iterations <= 0)
 			{
-				//GameObject currJoint = jointList[j];
-				//GameObject parentJoint = jointList[j - 1];
-
-				Vector3 dir = (jointList[j - 1].transform.position - jointList[j].transform.position).normalized;    //set the direction vector to place the parent joint inline
-
-				jointList[j - 1].transform.position = jointList[j].transform.position - (distances[j - 1] * dir); //set position of parent joint in the direction of the joint movement by the distance between
+				iter = 1;
+			}
+			else
+			{
+				iter = iterations;
 			}
 
-			jointList[0].transform.position = firstBasePos;      //move the beginning joint back to its root pos
+			Vector3 firstBasePos = OVRSkeletonNodes[jointList[0]].transform.position;    //record original root location
 
-			for (int j = 0; j < jointList.Count - 1; ++j)
+			for (int i = 0; i < iter; ++i)
 			{
-				//GameObject currJoint = jointList[j];
-				//GameObject childJoint = jointList[j + 1];
+				//Debug.Log("[OUR CODE] END JOINT NAME: " + OVRSkeletonNodes[jointList[(jointList.Count - 1)]].name);
 
-				Vector3 dir = (jointList[j + 1].transform.position - jointList[j].transform.position).normalized;
-				jointList[j + 1].transform.position = jointList[j].transform.position + (distances[j] * dir);
+				OVRSkeletonNodes[jointList[(jointList.Count - 1)]].transform.position = targetPos;    //move the endEffector to the target
+
+				//Debug.Log("[OUR CODE] Check One");
+
+				//go through the chain from end to beginning
+				for (int j = (jointList.Count - 1); j > 0; --j)     //has to be this weird way for parents
+				{
+					//GameObject currJoint = jointList[j];
+					//GameObject parentJoint = jointList[j - 1];
+					//Debug.Log("[OUR CODE] Current Joint: " + OVRSkeletonNodes[jointList[j]].name + "    Parent Joint: " + OVRSkeletonNodes[jointList[j - 1]].name);
+
+					Vector3 dir = (OVRSkeletonNodes[jointList[j - 1]].transform.position - OVRSkeletonNodes[jointList[j]].transform.position).normalized;    //set the direction vector to place the parent joint inline
+					OVRSkeletonNodes[jointList[j - 1]].transform.position = OVRSkeletonNodes[jointList[j]].transform.position - (distances[j - 1] * dir); //set position of parent joint in the direction of the joint movement by the distance between
+				}
+
+				OVRSkeletonNodes[jointList[0]].transform.position = firstBasePos;      //move the beginning joint back to its root pos
+
+				//go from beginning to end
+				for (int j = 0; j < jointList.Count - 1; ++j)
+				{
+					//GameObject currJoint = jointList[j];
+					//GameObject childJoint = jointList[j + 1];
+					//Debug.Log("[OUR CODE] Current Joint: " + OVRSkeletonNodes[jointList[j]].name + "    Child Joint: " + OVRSkeletonNodes[jointList[j + 1]].name);
+
+					Vector3 dir = (OVRSkeletonNodes[jointList[j + 1]].transform.position - OVRSkeletonNodes[jointList[j]].transform.position).normalized;
+					OVRSkeletonNodes[jointList[j + 1]].transform.position = OVRSkeletonNodes[jointList[j]].transform.position + (distances[j] * dir);
+				}
+
+				//Solver for effector
+				//then effector pos minus the pos of its child
+				//Vector3 offset = OVRSkeletonNodes[jointList[(jointList.Count - 1)]].transform.position - OVRSkeletonNodes[jointList[(jointList.Count - 2)]].transform.position;
+				Vector3 offset = OVRSkeletonNodes[jointList[(jointList.Count - 1)]].transform.position - targetPos;
+				if(offset.magnitude > effectorTolerance)
+                {
+					break;
+                }
+				
 			}
 		}
 	}
