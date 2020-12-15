da8050b51ca99c50f5f90110bf7799fa3e9c6a1a
Connor Ramsden <connor.ramsden@mymail.champlain.edu>
2020-12-14 20:51:04 -0500

    Initial work on HTR and live data capture, need to integrate with OVR

---
+++ AnimFinal/Assets/Scripts/LiveDataCaptureInterface.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+	public class FrameData
+	{
+		public Vector3 head;
+		public Vector3 handLeft;
+		public Vector3 handRight;
+	}
+
+	public class LiveDataCaptureInterface : MonoBehaviour
+	{
+		public List<FrameData> positionalData;
+		public List<FrameData> rotationalData;
+
+		public void PackPosition(Vector3 handLeft, Vector3 handRight, Vector3 head)
+		{
+		}
+
+		public void PackRotation(Vector3 handLeft, Vector3 handRight, Vector3 head)
+		{
+		}
+	}
+}
---
+++ AnimFinal/Assets/Scripts/VR/InputHandler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace.VR
+{
+	public class InputHandler : MonoBehaviour
+	{
+		// Positional data from the Headset / Controllers
+		public Vector3 leftControllerPosition;
+		public Vector3 rightControllerPosition;
+		public Vector3 headsetPosition;
+		
+		// Rotational data from the Headset / Controllers
+		public Vector3 leftControllerRotation;
+		public Vector3 rightControllerRotation;
+		public Vector3 headsetRotation;
+
+		// Assign controller prefabs to left and right respectively
+		// Set rotations to be Euler
+		public void Init()
+		{
+		}
+
+		// Acquire Pos & Rot data for both controllers & headset
+		public void CheckInput()
+		{
+		}
+
+		// Set the transform of the headset and controllers
+		// in real world to be translated to gamespace using VR input
+		public void AdjustControllerSpace()
+		{
+		}
+	}
+}
